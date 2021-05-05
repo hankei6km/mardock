e352f523e16cde911278ef39b968879d22793fd4
hankei6km <gh-hankei6km@outlook.jp>
2021-05-05 16:11:05 +0900

    Fix assetPrefix and basePath

--- next.config.js
+++ next.config.js
@@ -39,7 +39,7 @@ module.exports = (phase) => {
     : '';
   const assetPrefix =
     isStaging && process.env.STAGING_DIR
-      ? path.join('/', process.env.STAGING_DIR, _assetPrefix)
+      ? path.join(_assetPrefix, process.env.STAGING_DIR)
       : _assetPrefix;
   // lib/meta.ts 内で baseUrl を独自に設定しているので注意
   const basePath = assetPrefix;
