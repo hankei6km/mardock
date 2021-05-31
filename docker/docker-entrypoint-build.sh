450b21c801e5d256d375a96e6efb074c3f11a50e
hankei6km <gh-hankei6km@outlook.jp>
2021-06-01 02:43:17 +0900

    Set MARDOCK_CWD in Dockerfile

--- docker/docker-entrypoint-build.sh
+++ docker/docker-entrypoint-build.sh
@@ -5,5 +5,5 @@ SCRIPT_NAME="build"
 if test "$(id -u)" -eq 0; then
   exec su-exec "${MARDOCK_USER}" yarn --cwd "${MARDOCK_CWD}" "${SCRIPT_NAME}"
 else
-  exec sh -c yarn --cwd "${MARDOCK_CWD}" "${SCRIPT_NAME}"
+  exec yarn --cwd "${MARDOCK_CWD}" "${SCRIPT_NAME}"
 fi
--- docker/docker-entrypoint-start.sh
+++ docker/docker-entrypoint-start.sh
@@ -1,10 +1,9 @@
 #!/bin/ash
 
-echo "CWD: ${MARDOCK_CWD}"
 SCRIPT_NAME="start"
 
 if test "$(id -u)" -eq 0; then
   exec su-exec "${MARDOCK_USER}" yarn --cwd "${MARDOCK_CWD}" "${SCRIPT_NAME}"
 else
-  exec sh -c yarn --cwd "${MARDOCK_CWD}" "${SCRIPT_NAME}"
+  exec yarn --cwd "${MARDOCK_CWD}" "${SCRIPT_NAME}"
 fi
