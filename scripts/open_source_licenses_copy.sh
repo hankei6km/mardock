7907f9e8bfac34d388240ceef2a39af0ac7733ab
hankei6km <gh-hankei6km@outlook.jp>
2021-06-15 16:29:05 +0900

    Fix script in CodeSandbox

--- scripts/open_source_licenses_copy.sh
+++ scripts/open_source_licenses_copy.sh
@@ -5,6 +5,6 @@ PUBLIC_PATH="public"
 SRC_LICENSES_ZIP="open_source_licenses.zip"
 DEST_LICENSES_ZIP="${PUBLIC_PATH}/open_source_licenses.zip"
 
-test -e "${DEST_LICENSES_ZIP}" && rm "${DEST_LICENSES_ZIP}"
+test -e "${DEST_LICENSES_ZIP}" && rm "${DEST_LICENSES_ZIP}" || true 
 
-cp "${SRC_LICENSES_ZIP}" "${DEST_LICENSES_ZIP}"
+test -e "${SRC_LICENSES_ZIP}" && cp "${SRC_LICENSES_ZIP}" "${DEST_LICENSES_ZIP}" || true
